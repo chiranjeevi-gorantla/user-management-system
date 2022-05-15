a38b73c4b574b578760180c77325605ad8daaae3
chiranjeevi-gorantla <chiranjeevigorantla@gmail.com>
2022-05-15 16:35:13 -0400

    loading-modules

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
+const bodyparser = require("body-parser");
+const path = require("path");
+const exp = require("constants");
 
 const app = express();
 
@@ -9,6 +12,17 @@ const PORT = process.env.PORT || 3000;
 
 //log requests
 app.use(morgan("tiny"));
+
+//parse requests to body-parser
+app.use(bodyparser.urlencoded({ extended: true }));
+
+//set view engine
+app.set("view engine", "ejs");
+
+//load assets
+app.use("/css", express.static(path.resolve(__dirname, "assets/css")));
+app.use("/img", express.static(path.resolve(__dirname, "assets/img")));
+app.use("/js", express.static(path.resolve(__dirname, "assets/js")));
 
 app.get("/", (req, res) => {
   res.send("Crud Application");
