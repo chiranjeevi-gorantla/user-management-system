6299e5aea73a36d3dd5423c349ea427e3bebae3e
chiranjeevi-gorantla <chiranjeevigorantla@gmail.com>
2022-05-26 00:20:18 -0400

    createUser-operation

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -1,7 +1,34 @@
 var Usersdb = require("../model/model");
 
 // create and save new user
-exports.create = (req, res) => {};
+exports.create = (req, res) => {
+  // validate request
+  if (!req.body) {
+    res.status(400).send({ message: "Content cannot be empty!" });
+    return;
+  }
+
+  // new user
+  const user = new Usersdb({
+    name: req.body.name,
+    email: req.body.email,
+    gender: req.body.gender,
+    status: req.body.status,
+  });
+
+  //save user in the database
+  user
+    .save(user)
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occured while creating a create operation",
+      });
+    });
+};
 
 // retrieve and return all users & retrive and return a single user
 exports.find = (req, res) => {};
